5607650582d401dff8f3fe00f1670f16331fb1af
pawan dalvi <psdalvi98@gmail.com>
2022-04-24 23:31:56 +0530

    changes in iframe by pawan

--- src/test/java/Com/Test/Iframe.java
+++ src/test/java/Com/Test/Iframe.java
@@ -28,5 +28,26 @@ public class Iframe {
 		driver.switchTo().parentFrame();       //Switch to Main frame
 		
 	}
+	@Test
+	public void Iframe2() {
+		//frame(index)
+		//frame(name or id)
+		//frame(element)
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("file:///C:/Users/Dell/Documents/demoiframe.html");
+		driver.manage().window().maximize();
+		
+		driver.switchTo().frame("maven");    //Switch to frame from main page
+		
+		driver.switchTo().defaultContent();   //Switch to main page from iframe
+		
+		driver.switchTo().frame(1);           //Switch to frame from main page
+		
+		driver.switchTo().parentFrame();       //Switch to Main frame
+		
+	}
+
 
 }
